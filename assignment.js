e21c8c098db7bbedc462c8dd3090173966748555
moazzem852 <67514567+moazzem852@users.noreply.github.com>
2020-07-18 02:50:26 +0600

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -20,29 +20,29 @@ function woodCalculator(chair, table ,cot) {
 }
 
 
-
 function brickCalculator(floor) {
-    var bricksAmount = floor;
-    var bricksAmount1 =bricksAmount;
-    var bricksAmount2 = floor-bricksAmount1;
-    var bricksAmount3 = floor-bricksAmount2;
+    var feet = 1000;
+    step;
+    var step1;
+    var step2;
+    var step3;
 
-    if (floor <=10) {
-      bricksAmount1 = floor * 15000;
-      result = bricksAmount1;  
-    }
-     else if (floor > 10 && floor <= 20) {
-       bricksAmount2 = floor * 12000;
-      result = bricksAmount2;   
-     }
-     else if (floor > 20){
-         var bricksAmount3 = floor * 10000;
-         result = bricksAmount3;
-     }
-     var bricksAmount = bricksAmount1+bricksAmount2+bricksAmount3;
-      return bricksAmount; 
-       
- }
+         if (floor <=10) {
+             
+            step1= feet * 15 * floor;
+            return step1;
+         }
+         else if (floor >= 11 && floor <= 20) {
+             step2= feet * 15 * floor;
+             return step2;
+         }
+         else if (floor >= 21) {
+             step3 =feet * 10 * floor;
+             return step3;
+         }
+    var step = step1 + step2 + step3;
+    return step;
+}
 
 
 
